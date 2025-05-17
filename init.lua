99c1e2173a4b36a914d1b964b2d7a37e07ceb4b9
Sam Fredrickson <samfredrickson@gmail.com>
2025-05-16 23:53:22 -0700

    Set default tab behavior.

--- init.lua
+++ init.lua
@@ -104,6 +104,10 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
+-- Set default tab behavior
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
