7b1390467c5185067e0e7dd35e969fa8082873ca
Sam Fredrickson <samfredrickson@gmail.com>
2025-05-16 22:55:11 -0700

    Enable LSP for Go.

--- lua/custom/plugins/go.nvim.lua
+++ lua/custom/plugins/go.nvim.lua
@@ -6,10 +6,11 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
-    require('go').setup()
+    require('go').setup {
+      lsp_cfg = true,
+    }
   end,
   event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
-
