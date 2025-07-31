88aa1fdec47ece5093793d8d72fbfb5003b3f3f6
Sam Fredrickson <samfredrickson@gmail.com>
2025-07-31 01:44:14 -0700

    Install obsidian.nvim.

--- init.lua
+++ init.lua
@@ -157,6 +157,8 @@ vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 
+vim.o.conceallevel = 1
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
